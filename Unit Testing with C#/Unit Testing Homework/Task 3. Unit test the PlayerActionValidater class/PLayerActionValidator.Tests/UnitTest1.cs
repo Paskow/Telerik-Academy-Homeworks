c63091f2a930438ee0f3cc0e71f9132075e29634
Kiril Paskov <kiko0o7@abv.bg>
2016-07-27 14:38:20 +0300

    Added partly finished Task 3 to C# Unit Testing homework

---
+++ Unit Testing with C#/Unit Testing Homework/Task 3. Unit test the PlayerActionValidater class/PLayerActionValidator.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Santase.Logic.PlayerActionValidate;
+using Santase.Logic.Players;
+using Santase.Logic.Cards;
+using Santase.Logic.RoundStates;
+using System.Collections.Generic;
+
+namespace PLayerActionValidator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsValid_usingValidData_ShuldBeValid()
+        {
+            var actionValidator = new PlayerActionValidator();
+            ICollection<Card> playerCards = new List<Card>();
+            var deck = new Deck();
+            var context = new PlayerTurnContext(new FinalRoundState(new StateManager()), deck.TrumpCard, deck.CardsLeft, 3, 4);
+            bool result = actionValidator.IsValid(PlayerAction.PlayCard(new Card(CardSuit.Club, CardType.Jack)),
+                                   context,
+                                   playerCards);
+
+            playerCards.Add(new Card(CardSuit.Club, CardType.Jack));
+            context.FirstPlayedCard = null;
+                     
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void GetPossibleCardToPlay()
+        {
+            var actionValidator = new PlayerActionValidator();
+            
+        }
+    }
+}
