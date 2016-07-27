2b09780530faecc15b92b0679064f8f0f1b7a5b5
Kiril Paskov <kiko0o7@abv.bg>
2016-07-27 12:09:07 +0300

    Added Unit Testing Homework Task 2

---
+++ Unit Testing with C#/Unit Testing Homework/Task 2. SentaseGameTestDeckClass/README.md
@@ -0,0 +1,4 @@
+## Task 2. Unit test the `Deck` class from the `SantaseGameEngine`
+*   [Deck.cs](https://github.com/NikolayIT/SantaseGameEngine/blob/master/Source/Santase.Logic/Cards/Deck.cs)
+*   Use NUnit as a testing framework
+*   Write at least one [Parameterized Test](http://nunit.org/index.php?p=parameterizedTests&r=2.6.1)
---
+++ Unit Testing with C#/Unit Testing Homework/Task 2. SentaseGameTestDeckClass/TestDeckClass/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Santase.Logic.Cards;
+using NUnit.Framework;
+using Santase.Logic;
+namespace TestDeckClass
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void IntializeDeckClass_ShouldInitializeCorrectly()
+        {
+            var deck = new Deck();
+
+            Assert.AreEqual(24, deck.CardsLeft);
+        }
+
+        [Test]
+        public void GetNextCar_WhenThereIsNoCard_ShouldThrowException()
+        {
+            var deck = new Deck();
+
+            for (int i = 0; i < 24; i++)
+            {
+                deck.GetNextCard();
+            }
+
+            Assert.Throws<InternalGameException>(() => deck.GetNextCard());
+        }
+
+
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 5)]
+        [TestCase(0, 3)]
+        public void ChangeTrumbCard_ShouldChangeItCorrectly(int a, int b)
+        {
+            var deck = new Deck();
+            var card = new Card((CardSuit)a, (CardType)b);
+
+            deck.ChangeTrumpCard(card);
+
+            Assert.AreEqual(card, deck.TrumpCard);
+        }
+
+        [Test]
+        public void ChangeTrumbCard_WhenThereIsNoCardsLeft_ShouldChangeItCorrectly()
+        {
+            var deck = new Deck();
+            var card = new Card(CardSuit.Club, CardType.Nine);
+
+            for (int i = 0; i < 24; i++)
+            {
+                deck.GetNextCard();
+            }
+
+            deck.ChangeTrumpCard(card);
+
+            Assert.AreEqual(card, deck.TrumpCard);
+        }
+    }
+}
