485025510c594fbeb40bd771fc2af47b4e01cd3c
Kiril Paskov <kiko0o7@abv.bg>
2016-08-30 18:53:33 +0300

    Added Variables, Data, Expressions and Constants Homework

---
+++ High-Quality Code Part 1/Variables, Data, Expressions and Constants/Variable Naming And Usage/Class Size in CSharp/Size.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Size
+{
+    private double width;
+    private double height;
+
+    public Size(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double Width
+    {
+        get
+        {
+            return this.width;
+        }
+    }
+
+    public double Height
+    {
+        get
+        {
+            return this.height;
+        }
+    }
+
+    public static Size GetRotatedSize(
+        Size size, double angleToBeRotaed)
+    {
+        double cosinus = Math.Cos(angleToBeRotaed);
+        double sinus = Math.Sin(angleToBeRotaed);
+        double width = (cosinus * size.Width) + (sinus * size.Height);
+        double height = (sinus * size.Width) + (cosinus * size.Height);
+
+        return new Size(width, height);
+    }
+}
---
+++ High-Quality Code Part 1/Variables, Data, Expressions and Constants/Variable Naming And Usage/Method PrintStatistics in CSharp/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Method_PrintStatistics_in_CSharp
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double max = arr[0];
+
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            Console.WriteLine(max);
+
+            double min = arr[0];
+            
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            Console.WriteLine(min);
+
+            double sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                sum += arr[i];
+            }
+
+            double average = sum / count;
+
+            Console.WriteLine(average);
+        }
+    }
+}
